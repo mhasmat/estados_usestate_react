9a979894b8210bea1674b03756e20855457baa8d
mhasmat <mhasmat@gmail.com>
2024-09-05 19:47:25 -0300

    corrección pequeño error

--- ejemplos_de_clase/src/components/RenderCondicional.jsx
+++ ejemplos_de_clase/src/components/RenderCondicional.jsx
@@ -6,8 +6,8 @@ import icoVendido from "../assets/images/vendido.png"
 const RenderCondicional = () => {
   const [disponible, setDisponible] = useState(true);
 
-  const cartelDisponible = <div><img src={icoDisponible} height="200px"/></div>;
-  const cartelVendido = <div><img src={icoVendido} height="200px"/></div>;
+  const cartelDisponible = <div><img src={icoDisponible} height="200px" alt="disponible"/></div>;
+  const cartelVendido = <div><img src={icoVendido} height="200px" alt="vendido"/></div>;
 
   return (
     <div>
